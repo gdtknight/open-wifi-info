4af4331ee4db08a8404289637f16bcbb45ab4fd6
gdtknight <gdtknight@naver.com>
2023-02-13 22:04:30 +0900

    project 생성 및 초기 설정 완료
    [Dependencies]
    
    - web

---
+++ src/main/java/io/github/gdtknight/open_wifi_info/OpenWifiInfoApplication.java
@@ -0,0 +1,13 @@
+package io.github.gdtknight.open_wifi_info;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenWifiInfoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OpenWifiInfoApplication.class, args);
+  }
+
+}
---
+++ src/test/java/io/github/gdtknight/open_wifi_info/OpenWifiInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.gdtknight.open_wifi_info;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenWifiInfoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
