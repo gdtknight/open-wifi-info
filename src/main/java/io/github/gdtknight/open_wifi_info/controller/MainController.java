f66f302158b2ed8a0e180e4f0bde2af965c9942f
gdtknight <gdtknight@naver.com>
2023-02-21 20:00:19 +0900

    #1 - add MainController

---
+++ src/main/java/io/github/gdtknight/open_wifi_info/controller/MainController.java
@@ -0,0 +1,17 @@
+package io.github.gdtknight.open_wifi_info.controller;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class MainController {
+
+  @GetMapping
+  public String mainPage() throws IOException {
+    return "index";
+  }
+}
